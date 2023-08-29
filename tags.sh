534e9921eb6df215364986a02fb186a8ff8402ca
naosense <pingao777@gmail.com>
2023-08-29 17:14:25 +0800

    兼容tag没有加空格的情况

--- tags.sh
+++ tags.sh
@@ -7,7 +7,7 @@ tags=()
 for file in source/_posts/*.md; do
     # 使用sed命令提取标签行，然后使用awk将标签提取出来
     tag_line=$(sed -n '/^tags:/p' "$file")
-    tags+=( $(echo "$tag_line" | awk -F 'tags: ' '{print $2}' | tr -d '[],' | tr ' ' '\n') )
+    tags+=( $(echo "$tag_line" | awk -F 'tags: ' '{print $2}' | tr -d '[] ' | tr ',' '\n') )
 done
 
 # 使用sort命令对标签进行排序并去重
